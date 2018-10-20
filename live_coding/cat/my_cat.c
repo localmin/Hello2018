9876f0e7f2df66fda8c9a78f624709b306f1c5c9
localmin <localmin9201009@gmail.com>
2018-10-20 23:11:01 +0900

    Add the comment about opne command

--- live_coding/cat/my_cat.c
+++ live_coding/cat/my_cat.c
@@ -7,9 +7,9 @@ int main(int argc, char **argv){
     uint8_t buf[4096];// 4096 = (0x1000)_16t uint8_t: unsigned short int :16bit
     ssize_t nread; //size_tはmallocに渡される引数の型であり、ssize_tはread()とwrite()の戻り値の型です
     
-    int fd = open(argv[1], O_RDONLY);
+    int fd = open(argv[1], O_RDONLY);//file opne
 
-    while((nread = read(fd, buf, sizeof(buf))) > 0)
+    while((nread = read(fd, buf, sizeof(buf))) > 0) //0(標準入力からファイルへ)
         write(1, buf, nread);
     return 0;
 }

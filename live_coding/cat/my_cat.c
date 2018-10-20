68b212dcc516632ef4e1f9c8f3c617029e1d185f
localmin <localmin9201009@gmail.com>
2018-10-20 23:07:12 +0900

    Implemetned file opne function

--- live_coding/cat/my_cat.c
+++ live_coding/cat/my_cat.c
@@ -4,9 +4,12 @@
 #include <fcntl.h>
 
 int main(int argc, char **argv){
-    uint8_t buf[4096];
-    ssize_t nread;
-    while((nread = read(0, buf, sizeof(buf))) > 0)
+    uint8_t buf[4096];// 4096 = (0x1000)_16t uint8_t: unsigned short int :16bit
+    ssize_t nread; //size_tはmallocに渡される引数の型であり、ssize_tはread()とwrite()の戻り値の型です
+    
+    int fd = open(argv[1], O_RDONLY);
+
+    while((nread = read(fd, buf, sizeof(buf))) > 0)
         write(1, buf, nread);
     return 0;
 }

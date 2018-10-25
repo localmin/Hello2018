fe01daf9cb3adfc5fb23ccdb6b2988cca61729f2
localmin <localmin9201009@gmail.com>
2018-10-25 23:16:32 +0900

    Use perror

--- live_coding/cat/my_cat.c
+++ live_coding/cat/my_cat.c
@@ -2,6 +2,8 @@
 #include <inttypes.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv){
     uint8_t buf[4096];// 4096 = (0x1000)_16t uint8_t: unsigned short int :16bit
@@ -9,6 +11,10 @@ int main(int argc, char **argv){
     
     int fd = open(argv[1], O_RDONLY);//file opne fdは成功したらファイルハンドル，エラー時は-1を返す
 
+    if(fd < 0){
+        perror("open");
+        exit(-1);
+    }
     while((nread = read(fd, buf, sizeof(buf))) > 0) //0(標準入力からファイルへ)
         write(1, buf, nread);
     return 0;

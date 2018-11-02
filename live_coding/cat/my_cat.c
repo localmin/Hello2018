eff3cad68bc8db9297efcb078b38f897729d2b38
localmin <localmin9201009@gmail.com>
2018-11-02 14:49:43 +0900

    Change the error message bu using fprintf

--- live_coding/cat/my_cat.c
+++ live_coding/cat/my_cat.c
@@ -4,6 +4,8 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <errno.h>
 
 int main(int argc, char **argv){
     uint8_t buf[4096];// 4096 = (0x1000)_16t uint8_t: unsigned short int :16bit
@@ -12,8 +14,8 @@ int main(int argc, char **argv){
     int fd = open(argv[1], O_RDONLY);//file opne fdは成功したらファイルハンドル，エラー時は-1を返す
 
     if(fd < 0){
-        perror("open");
-        exit(-1);
+        fprintf(stderr, "can not open %s : %s\n", argv[1], strerror(errno));
+        exit(1);
     }
     while((nread = read(fd, buf, sizeof(buf))) > 0) //0(標準入力からファイルへ)
         write(1, buf, nread);

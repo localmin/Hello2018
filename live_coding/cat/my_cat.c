53e3f084936aa8f791b701f9d502a094a2d91821
localmin <localmin9201009@gmail.com>
2018-11-05 17:47:32 +0900

    Add the condtion about argc

--- live_coding/cat/my_cat.c
+++ live_coding/cat/my_cat.c
@@ -10,14 +10,17 @@
 int main(int argc, char **argv){
     uint8_t buf[4096];// 4096 = (0x1000)_16t uint8_t: unsigned short int :16bit
     ssize_t nread; //size_tはmallocに渡される引数の型であり、ssize_tはread()とwrite()の戻り値の型です
-    
-    int fd = open(argv[1], O_RDONLY);//file opne fdは成功したらファイルハンドル，エラー時は-1を返す
+    int fd = STDIN_FILENO; 
 
-    if(fd < 0){
-        fprintf(stderr, "can not open %s : %s\n", argv[1], strerror(errno));
-// perror はvoidを返す(error messageは標準エラー出力), strerrorは文字列を返す
-// printfは出力先をstdoutに固定している 
-        exit(1);
+    if(argc > 1){
+        fd = open(argv[1], O_RDONLY);//file opne fdは成功したらファイルハンドル，エラー時は-1を返す
+
+        if(fd < 0){
+            fprintf(stderr, "can not open %s : %s\n", argv[1], strerror(errno));
+        // perror はvoidを返す(error messageは標準エラー出力), strerrorは文字列を返す
+        // printfは出力先をstdoutに固定している 
+            exit(1);
+        }
     }
     while((nread = read(fd, buf, sizeof(buf))) > 0) //0(標準入力からファイルへ)
         write(1, buf, nread);

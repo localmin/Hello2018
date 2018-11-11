4d0119144dfe5c1a8b1905aa23817a6a6aeb7fdf
localmin <localmin9201009@gmail.com>
2018-11-11 23:59:00 +0900

    Initial commit of making wc command

---
+++ live_coding/wc/README.md
@@ -0,0 +1,1 @@
+### copy [ライブコーディング wcコマンド](https://www.youtube.com/watch?v=7gnpR0Dinqs)
---
+++ live_coding/wc/my_wc.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char **argv){
+    char buf[4096];
+    int nread = 0;
+
+    printf("%d\n", nread);
+    return 0;
+}

ee7484b4cb91cf684b36be0dde7701f464fdf3eb
localmin <localmin9201009@gmail.com>
2018-11-12 22:52:21 +0900

    Implemeted the easy feature to get numnter of line

--- live_coding/wc/my_wc.c
+++ live_coding/wc/my_wc.c
@@ -2,8 +2,14 @@
 
 int main(int argc, char **argv){
     char buf[4096];
-    int nread = 0;
+    int nlines = 0;
 
-    printf("%d\n", nread);
+    size_t nread;
+    while((nread = fread(buf, 1, sizeof(buf), stdin)) > 0)
+        for(size_t i = 0; i < nread; i++)
+            if(buf[i] == '\n')
+                nlines++;
+
+    printf("%d\n", nlines);
     return 0;
 }

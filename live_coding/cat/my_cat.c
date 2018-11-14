2ebea1638842f995c4618a311f51df6805b713fa
localmin <localmin9201009@gmail.com>
2018-11-14 23:07:58 +0900

    Fix the condtion for no arguments of cat command

--- live_coding/cat/my_cat.c
+++ live_coding/cat/my_cat.c
@@ -11,19 +11,23 @@ int main(int argc, char **argv){
     uint8_t buf[4096];// 4096 = (0x1000)_16t uint8_t: unsigned short int :16bit
     ssize_t nread; //size_tはmallocに渡される引数の型であり、ssize_tはread()とwrite()の戻り値の型です
 
-//    for(int i=1; argv[i] != NULL; i++){
-        int fd = STDIN_FILENO; 
-        if(argc > 1){
-            fd = open(argv[1], O_RDONLY);//file opne fdは成功したらファイルハンドル，エラー時は-1を返す
+    int fd = STDIN_FILENO; 
+    if(argc > 1){
+         for(int i=1; argv[i] != NULL; i++){
+            fd = open(argv[i], O_RDONLY);//file opne fdは成功したらファイルハンドル，エラー時は-1を返す
     
             if(fd < 0){
-                fprintf(stderr, "can not open %s : %s\n", argv[1], strerror(errno));
+                fprintf(stderr, "can not open %s : %s\n", argv[i], strerror(errno));
             // perror はvoidを返す(error messageは標準エラー出力), strerrorは文字列を返す
             // printfは出力先をstdoutに固定している 
             }
-        }
+          nread = read(fd, buf, sizeof(buf));
+          write(1, buf, nread);
+         }
+     }else{
         while((nread = read(fd, buf, sizeof(buf))) > 0) //ファイルディスクリプタを読み込む
             write(1, buf, nread);
- //   }
+     }
+
     return 0;
 }

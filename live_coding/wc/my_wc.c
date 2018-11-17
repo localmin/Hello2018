cfd4c3a9bb0a56f8a6b6c3521e7bf5492ec5176b
localmin <localmin9201009@gmail.com>
2018-11-17 22:55:12 +0900

    Make the function counting the lines

--- live_coding/wc/my_wc.c
+++ live_coding/wc/my_wc.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 
-int main(int argc, char **argv){
+int nlines = 0;
+
+static void wc(FILE *in){
     char buf[4096];
-    int nlines = 0;
 
     size_t nread;
     while((nread = fread(buf, 1, sizeof(buf), stdin)) > 0)
         for(size_t i = 0; i < nread; i++)
             if(buf[i] == '\n')
                 nlines++;
+}
 
+int main(int argc, char **argv){
+    wc(stdin);
     printf("%d\n", nlines);
     return 0;
 }

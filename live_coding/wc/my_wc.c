1c885a4dd0a56fe3b442dbf8bd93a2d7a403e0bc
localmin <localmin9201009@gmail.com>
2018-11-18 23:19:33 +0900

    Screen the filename

--- live_coding/wc/my_wc.c
+++ live_coding/wc/my_wc.c
@@ -2,18 +2,27 @@
 
 int nlines = 0;
 
-static void wc(FILE *in){
+static void wc(FILE *in, char *filename){
     char buf[4096];
+    int nl = 0;
 
     size_t nread;
     while((nread = fread(buf, 1, sizeof(buf), in)) > 0)
         for(size_t i = 0; i < nread; i++)
             if(buf[i] == '\n')
-                nlines++;
+                nl++;
+
+    if(filename)
+        printf("%d %s\n", nl, filename);
+    else
+        printf("%d \n", nl);
+
+    nlines += nl;
+
 }
 
 int main(int argc, char **argv){
-    wc(stdin);
+    wc(stdin, "foo");
     printf("%d\n", nlines);
     return 0;
 }

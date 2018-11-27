b15093789d5abc0378a1eec1ea3e87cb1b55d3f4
localmin <localmin9201009@gmail.com>
2018-11-27 22:51:48 +0900

    Change the test script

--- live_coding/wc/test.sh
+++ live_coding/wc/test.sh
@@ -2,12 +2,12 @@
 set -e 
 gcc -o my_wc my_wc.c
 
-[ "`./my_wc test1`" = '5 test1' ]
+[ "`./my_wc test1`" = '5 14 83 test1' ]
 
-[ "`./my_wc test2`" = '1 test2' ]
+[ "`./my_wc test2`" = '1 1 23 test2' ]
 
-[ "`./my_wc test1 test2`" = '5 test1
-1 test2
-6 total' ]
+[ "`./my_wc test1 test2`" = '5 14 83 test1
+1 1 23 test2
+6 15 106 total' ]
 
 echo Done

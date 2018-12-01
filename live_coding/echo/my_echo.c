43805c892f80db6b9c87d53a8ef67241dd6a90ae
localmin <localmin9201009@gmail.com>
2018-12-01 22:53:29 +0900

    Implement unescape

--- live_coding/echo/my_echo.c
+++ live_coding/echo/my_echo.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
 
+static void unescape(char* p){
+    for(; *p;  p++){
+        if(*p == '\n' || p[1] =='\0'){
+            printf("%c", *p);
+            continue;
+        }
+
+        p++;
+        if(*p == 'n'){
+            printf("\n");
+        }else if(*p == 't'){
+            printf("\t");
+        }else{
+            printf("\\%c", *p);
+        }
+    }
+}
+
 int main(int argc, char** argv){
     char** args = argv + 1;
     for(char** p = argv + 1; *p; p++)

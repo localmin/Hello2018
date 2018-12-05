cb89954bb6b3d618dde676ab2e183e3381abebfa
localmin <localmin9201009@gmail.com>
2018-12-05 22:28:56 +0900

    Finish echo command

--- live_coding/echo/my_echo.c
+++ live_coding/echo/my_echo.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
+#include <string.h>
 
 static void unescape(char* p){
     for(; *p;  p++){
-        if(*p == '\n' || p[1] =='\0'){
+        if(*p != '\\' || p[1] =='\0'){
             printf("%c", *p);
             continue;
         }
 
         p++;
-        if(*p == 'n'){
+        if(*p == 'n')
             printf("\n");
-        }else if(*p == 't'){
+        else if(*p == 't')
             printf("\t");
-        }else{
+        else
             printf("\\%c", *p);
-        }
+        
     }
 }
 
@@ -22,11 +23,21 @@ int main(int argc, char** argv){
     if(argc == 1)
         return 0;
 
-
     char** args = argv + 1;
-    for(char** p = argv + 1; *p; p++)
-        printf("%s%s",(p == argv ? "": " " ), *p);
-        //printf("%s ", *p);
+
+    if(strcmp(args[0], "-e") == 0){
+        args++;
+        for(char** p = args; *p; p++){
+            if(p != args)
+                printf(" ");
+            unescape(*p);
+        }
+
+    }else{
+        for(char** p = args; *p; p++)
+            printf("%s%s",(p == args ? "": " " ), *p);
+    }
+
     printf("\n");
     return 0;
 }

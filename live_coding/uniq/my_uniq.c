c5e93d28a7fd543f302cef97c512b7bedfcc4a10
localmin <localmin9201009@gmail.com>
2018-12-30 19:28:46 +0900

    Add the pointer for file input/output

--- live_coding/uniq/my_uniq.c
+++ live_coding/uniq/my_uniq.c
@@ -1,7 +1,14 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char* argv[]){
+    FILE* fp = NULL;
+    if(argc < 2){
+        fp = stdin;
+    }else{
+        fp = fopen(argv[1], "rb");
+    }
 
-    return 0;
+    fclose(fp);
 }
